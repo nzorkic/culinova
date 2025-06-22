6fdbbefc36b3906ae0653eb0f6e05a9531722fa5
Nikola Zorkic <zorkic.nikola@icloud.com>
2025-06-22 13:54:37 +0200

    Initial Commit

---
+++ Culinova/CulinovaApp.swift
@@ -0,0 +1,17 @@
+//
+//  CulinovaApp.swift
+//  Culinova
+//
+//  Created by Nikola Zorkic on 22. 6. 2025..
+//
+
+import SwiftUI
+
+@main
+struct CulinovaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CulinovaTests/CulinovaTests.swift
@@ -0,0 +1,17 @@
+//
+//  CulinovaTests.swift
+//  CulinovaTests
+//
+//  Created by Nikola Zorkic on 22. 6. 2025..
+//
+
+import Testing
+@testable import Culinova
+
+struct CulinovaTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
